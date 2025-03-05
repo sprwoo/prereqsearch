a5df04f7b19345691c5f9fcfae9f49f15bdbfb9e
Daniel Yang <veryshortshorty@gmail.com>
2025-03-04 19:18:38 -0500

    pdf to image to claude

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 from dotenv import load_dotenv, dotenv_values 
 import requests
+from pdfextract import pdf_to_base64 
 
 load_dotenv() 
 apikey = os.getenv("apikey")
@@ -13,12 +14,30 @@ headers = {
     'content-type' : 'application/json'
 }
 
+string = pdf_to_base64()
+
 payload = {
     "model" : "claude-3-7-sonnet-20250219",
     "max_tokens": 1024,
     "messages": [
-        {"role": "user", "content": "can anthropic read images?"}
-    ]
+        {
+            "role": "user", 
+            "content": [
+                {
+                    "type" : "image",
+                    "source" : {
+                        "type" : "base64",
+                        "media_type" : "image/png",
+                        "data" : string,
+                    },
+                },
+                {
+                    "type" : "text",
+                    "text" : "What is the title of this paper?"
+                }
+            ],
+        }
+    ],
 }
 
 r = requests.post(url, json=payload, headers=headers)
--- pdfextract.py
+++ pdfextract.py
@@ -1,6 +1,7 @@
 import fitz 
+import base64
 
-if __name__ == "__main__":
+def pdf_to_base64():
     path = "./attention.pdf"
 
     with fitz.open(path) as doc:
@@ -10,10 +11,21 @@ if __name__ == "__main__":
 
     with fitz.open(path) as doc:
         for page in doc:
-            pix = page.get_pixmap()
-            pix.save("page-%i.png" % page.number)
-            # to save this and put this into the ai, i think there are two ways
-            #   - turn it into base64 then send it to claude
-            #   - send the png into a database, get a url link, then send it to claude
+            image = page.get_pixmap()
+            image_data = image.tobytes("png")
+
+            image_b64 = base64.b64encode(image_data)
+            image_b64_string = image_b64.decode('utf-8')
+
+            return image_b64_string
+            
+            # print(image_b64_string)
+            # pix.save("page-%i.png" % page.number)
+        # to save this and put this into the ai, i think there are two ways
+        #   - turn it into base64 then send it to claude
+        #   - send the png into a database, get a url link, then send it to claude
+
+        # I think it would be much easier and faster to turn it into base64
+        
     
-    print(pymupdf_text)
+    # print(pymupdf_text)
